64854c0f54550cac4e130bbb705285401b90c0e7
Felix Hellman <felixhellman.ro@gmail.com>
2020-08-15 21:14:30 +0200

    Add failing control flow example

--- scripts/dummy.gd
+++ scripts/dummy.gd
@@ -25,3 +25,23 @@ func match_conditional(condition):
 		_:
 			print("Defualt!")
 	print("End")
+
+func multi_conditional(condition_a, condition_b):
+	match condition_a:
+		1:
+			if condition_b in range(0, 10):
+				match condition_b:
+					5:
+						print("1-5")
+					_:
+						print("1-Deff")
+		2:
+			print("2")
+		3:
+			match condition_b:
+				1:
+					print("3-1")
+				2:
+					print("3-2")
+				_:
+					print("3-deff")
